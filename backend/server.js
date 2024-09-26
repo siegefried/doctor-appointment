636b3ea0f76cc0539815a5edb4fb0993a3e278f8
Josh Lim <justdoit.josh@gmail.com>
2024-09-27 03:12:14 +0800

    test: testing solution for manual url entry on deploy

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(
 app.use(express.static("../frontend/dist"));
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.use("/api/users", usersRouter);
